5723ad17fe36a3b5012bdd50eb3507ce0172ca8a
Kepler-442b <rosewwross@gmail.com>
2020-03-09 11:45:29 +0900

    initial settings

---
+++ my_settings.py
@@ -0,0 +1,19 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'necktidal',
+        'USER': 'root',
+        'PASSWORD': '274835',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = {
+'secret':'necktidal588'}
+
+
+ALGORITHM = 'HS256'
+
+
+
